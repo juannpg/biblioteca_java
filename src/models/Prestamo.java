c09c20c439cea65131c0a4c3b8ba75bcc191c3a1
juannpg <juan.pasgut.workspace@gmail.com>
2025-04-01 10:28:24 +0200

    modelo prestamo

--- src/models/Prestamo.java
+++ src/models/Prestamo.java
@@ -1,4 +1,24 @@
 package models;
 
-public class Prestamo {}
+public class Prestamo {
+	private Libro libro;
+	private Socio socio;
+	private String fechaInicio;
+	private String fechaFin;
+	private String fechaDevolucion;
 
+  	public Prestamo(
+  		Libro libro,
+  		Socio socio,
+  		String fechaInicio,
+  		String fechaFin,
+  		String fechaDevolucion
+  	) {
+  		super();
+    	this.libro = libro;
+    	this.socio = socio;
+    	this.fechaInicio = fechaInicio;
+    	this.fechaFin = fechaFin;
+    	this.fechaDevolucion = fechaDevolucion;
+  	}
+}
