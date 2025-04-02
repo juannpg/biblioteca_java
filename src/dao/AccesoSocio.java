009cdb8b01b6246178fd8886d7989f87385a1d4f
danpanzgz <danbolocan13@gmail.com>
2025-04-02 13:25:54 +0200

    AccesoSocio actualizado manualmente con los cambiosn de las demas ramas

--- src/dao/AccesoSocio.java
+++ src/dao/AccesoSocio.java
@@ -14,141 +14,317 @@ import org.sqlite.SQLiteConfig;
 import config.ConfigSQLite;
 import entrada.Teclado;
 import exceptions.BDException;
+import exceptions.SocioException;
+import models.Libro;
 import models.Socio;
 
 public class AccesoSocio {
-	// 1.INSERTAR SOCIO
-	public static boolean añadirSocio(Socio socio) throws BDException {
-		PreparedStatement ps = null;
-		Connection conexion = null;
-		int resultados = 0;
-		try {
-			// Conexión a la base de datos
-			conexion = ConfigSQLite.abrirConexion();
-			String query = "INSERT INTO socio (dni, nombre, domicilio, telefono, correo) " + "VALUES ( ?, ?, ?, ?)";
+	//1.INSERTAR SOCIO
+	public static boolean agregarSocio(Socio socio) throws BDException {
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    int resultados = 0;
+	    try {
+	        // Conexión a la base de datos
+	        conexion = ConfigSQLite.abrirConexion();
+	        String query = "INSERT INTO socio (dni, nombre, domicilio, telefono, correo) "
+	                     + "VALUES ( ?, ?, ?, ?)";
 
-			ps = conexion.prepareStatement(query);
-			ps.setString(1, socio.getDni());
-			ps.setString(2, socio.getNombre());
-			ps.setString(3, socio.getDomicilio());
-			ps.setString(4, socio.getTelefono());
-			ps.setString(5, socio.getCorreo());
+	        ps = conexion.prepareStatement(query);
+	        ps.setString(1, socio.getDni());
+	        ps.setString(2, socio.getNombre());
+	        ps.setString(3, socio.getDomicilio());
+	        ps.setString(4, socio.getTelefono());
+	        ps.setString(5, socio.getCorreo());
 
-			resultados = ps.executeUpdate();
-		} catch (SQLException e) {
-			throw new BDException(BDException.ERROR_QUERY + e.getMessage());
-		} finally {
-			if (conexion != null) {
-				ConfigSQLite.cerrarConexion(conexion);
-			}
-		}
-		return resultados == 1;
+	        resultados = ps.executeUpdate();
+	    } catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return resultados == 1;
 	}
+	//2. ELIMINAR SOCIO POR CODIGO
+	public static boolean eliminarSocio(int codigoSocio) throws BDException, SocioException {
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    int resultados = 0;
+	    try {
+	        // Conexión a la base de datos
+	        conexion = ConfigSQLite.abrirConexion();
+	        String query = "DELETE FROM socio WHERE codigo=?";
 
-	// 2. ELIMINAR SOCIO POR CODIGO
-	public static boolean eliminarSocio(int codigoSocio) throws BDException {
-		PreparedStatement ps = null;
-		Connection conexion = null;
-		int resultados = 0;
-		try {
-			// Conexión a la base de datos
-			conexion = ConfigSQLite.abrirConexion();
-			String query = "DELETE FROM socio WHERE codigo=?";
+	        ps = conexion.prepareStatement(query);
+	        ps.setInt(1,codigoSocio);
 
-			ps = conexion.prepareStatement(query);
-			ps.setInt(1, codigoSocio);
-
-			resultados = ps.executeUpdate();
-		} catch (SQLException e) {
-			throw new BDException(BDException.ERROR_QUERY + e.getMessage());
-		} finally {
-			if (conexion != null) {
-				ConfigSQLite.cerrarConexion(conexion);
-			}
-		}
-		return resultados == 1;
+	        resultados = ps.executeUpdate();
+	        
+	        if (resultados == 0) {
+	        	throw new SocioException(SocioException.ERROR_NOSOCIO);
+	        }
+	        if (esPrestatario(codigoSocio)) {
+	        	throw new SocioException(SocioException.ERROR_SOCIO_PRESTAMO);
+	        }
+	    } catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return resultados == 1;
 	}
-
-	// 3. CONSULTAR TODOS LOS SOCIOS
-	public static ArrayList<Socio> consultarTodosSocios() throws BDException {
+	//3. CONSULTAR TODOS LOS SOCIOS
+	public static ArrayList<Socio> consultarTodosSocios() throws BDException, SocioException{
 		ArrayList<Socio> todosSocios = new ArrayList<Socio>();
-		PreparedStatement ps = null;
-		Connection conexion = null;
-		try {
-			conexion = ConfigSQLite.abrirConexion();
-			String queryString = "SELECT * FROM socio";
-			ps = conexion.prepareStatement(queryString);
-			ResultSet resultados = ps.executeQuery();
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    try {
+	    	conexion = ConfigSQLite.abrirConexion();
+	    	 String queryString = "SELECT * FROM socio";
+	    	 ps = conexion.prepareStatement(queryString);
+	    	 ResultSet resultados = ps.executeQuery();
+	    	 
+	    	 
+	    	 
+	    	 while (resultados.next()) {
+	    		 int codigoSocio = resultados.getInt("codigo");
+	    		 String dniSocio = resultados.getString("dni");
+	    		 String nombreSocio = resultados.getString("nombre");
+	    		 String domicilioSocio = resultados.getString("domicilio");
+	    		 String telefonoSocio = resultados.getString("telefono");
+	    		 String correoSocio = resultados.getString("correo");
 
-			while (resultados.next()) {
-				int codigoSocio = resultados.getInt("codigo");
-				String dniSocio = resultados.getString("dni");
-				String nombreSocio = resultados.getString("nombre");
-				String domicilioSocio = resultados.getString("domicilio");
-				String telefonoSocio = resultados.getString("telefono");
-				String correoSocio = resultados.getString("correo");
+	    		 Socio socio = new Socio(codigoSocio, dniSocio, nombreSocio, domicilioSocio, telefonoSocio, correoSocio);
+	    		 todosSocios.add(socio);
 
-				Socio socio = new Socio(codigoSocio, dniSocio, nombreSocio, domicilioSocio, telefonoSocio, correoSocio);
-				todosSocios.add(socio);
-
-			}
-		} catch (SQLException e) {
-			throw new BDException(BDException.ERROR_QUERY + e.getMessage());
-		} finally {
-			if (conexion != null) {
-				ConfigSQLite.cerrarConexion(conexion);
-			}
-		}
-		return todosSocios;
+	    	 }
+	    	 if (todosSocios.isEmpty()) {
+	    		 throw new SocioException(SocioException.ERROR_BDEmpty);
+	    	 }
+	    }catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return todosSocios;
 	}
-
-	// 4. CONSULTAR SOCIO POR LOCALIDAD(parametro) ORDENADOS POR NOMBRE ASC
-	public static ArrayList<Socio> consultarSociosLocalidad(String localidad) throws BDException {
+	//4. CONSULTAR SOCIO POR LOCALIDAD(parametro) ORDENADOS POR NOMBRE ASC
+	public static ArrayList<Socio> consultarSociosLocalidad(String localidad) throws BDException, SocioException{
 		ArrayList<Socio> todosSociosLocalidad = new ArrayList<Socio>();
-		PreparedStatement ps = null;
-		Connection conexion = null;
-		try {
-			conexion = ConfigSQLite.abrirConexion();
-			String queryString = "SELECT * FROM socio WHERE localidad = ?";
-			ps = conexion.prepareStatement(queryString);
-			ps.setString(1, localidad);
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    try {
+	    	conexion = ConfigSQLite.abrirConexion();
+	    	 String queryString = "SELECT * FROM socio WHERE domicilio = ? ORDER BY nombre ASC";
+	    	 ps = conexion.prepareStatement(queryString);
+		     ps.setString(1, localidad);
 
-			ResultSet resultados = ps.executeQuery();
+	    	 ResultSet resultados = ps.executeQuery();
+	    	 
+	    	 while (resultados.next()) {
+	    		 int codigoSocio = resultados.getInt("codigo");
+	    		 String dniSocio = resultados.getString("dni");
+	    		 String nombreSocio = resultados.getString("nombre");
+	    		 String domicilioSocio = resultados.getString("domicilio");
+	    		 String telefonoSocio = resultados.getString("telefono");
+	    		 String correoSocio = resultados.getString("correo");
 
-			while (resultados.next()) {
-				int codigoSocio = resultados.getInt("codigo");
-				String dniSocio = resultados.getString("dni");
-				String nombreSocio = resultados.getString("nombre");
-				String domicilioSocio = resultados.getString("domicilio");
-				String telefonoSocio = resultados.getString("telefono");
-				String correoSocio = resultados.getString("correo");
+	    		 Socio socio = new Socio(codigoSocio, dniSocio, nombreSocio, domicilioSocio, telefonoSocio, correoSocio);
+	    		 todosSociosLocalidad.add(socio);
 
-				Socio socio = new Socio(codigoSocio, dniSocio, nombreSocio, domicilioSocio, telefonoSocio, correoSocio);
-				todosSociosLocalidad.add(socio);
-
-			}
-		} catch (SQLException e) {
-			throw new BDException(BDException.ERROR_QUERY + e.getMessage());
-		} finally {
-			if (conexion != null) {
-				ConfigSQLite.cerrarConexion(conexion);
-			}
-		}
-		return todosSociosLocalidad;
+	    	 }
+	    	 if (todosSociosLocalidad.isEmpty()) {
+	    		 throw new SocioException(SocioException.ERROR_SOCIO_LOCALIDAD);
+	    	 }
+	    }catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return todosSociosLocalidad;
 	}
-	// 5. CONSULTAR SOCIOS SIN PRESTAMOS
-	// public static ArrayList<Socio> consultarSociosNoPrestatario(){
+	//5. CONSULTAR SOCIOS SIN PRESTAMOS
+	//dar una vuelta
+	public static ArrayList<Socio> consultarSociosNoPrestatario() throws BDException, SocioException{
+		ArrayList<Socio> todosSociosNP = new ArrayList<Socio>();
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    try {
+	    	conexion = ConfigSQLite.abrirConexion();
+	    	 String queryString = "SELECT * FROM socio JOIN prestamo ON (socio.codigo = prestamo.codigo_socio) WHERE prestamo.fecha_devolucion IS NULL;";
+	    	 ps = conexion.prepareStatement(queryString);
 
+	    	 ResultSet resultados = ps.executeQuery();
+	    	 
+	    	 while (resultados.next()) {
+	    		 int codigoSocio = resultados.getInt("codigo");
+	    		 String dniSocio = resultados.getString("dni");
+	    		 String nombreSocio = resultados.getString("nombre");
+	    		 String domicilioSocio = resultados.getString("domicilio");
+	    		 String telefonoSocio = resultados.getString("telefono");
+	    		 String correoSocio = resultados.getString("correo");
+
+	    		 Socio socio = new Socio(codigoSocio, dniSocio, nombreSocio, domicilioSocio, telefonoSocio, correoSocio);
+	    		 todosSociosNP.add(socio);
+
+	    	 }
+	    	 if (todosSociosNP.isEmpty()) {
+	    		 throw new SocioException(SocioException.ERROR_SOCIO_NOPRESTAMO);
+	    	 }
+	    }catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return todosSociosNP;
+	}
+	//6. CONSULTAR SOCIOS CON PRESTAMOS en una fecha
+	public static ArrayList<Socio> consultarSociosPrestatario(String fecha) throws BDException, SocioException{
+		ArrayList<Socio> todosSocioSP = new ArrayList<Socio>();
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    try {
+	    	conexion = ConfigSQLite.abrirConexion();
+	    	 String queryString = "SELECT * FROM socio JOIN prestamo ON (socio.codigo = prestamo.codigo_socio) WHERE prestamo.fecha_inicio = ?;";
+	    	 ps = conexion.prepareStatement(queryString);
+	    	 ps.setString(1, fecha);
+	    	 ResultSet resultados = ps.executeQuery();
+	    	 
+	    	 while (resultados.next()) {
+	    		 int codigoSocio = resultados.getInt("codigo");
+	    		 String dniSocio = resultados.getString("dni");
+	    		 String nombreSocio = resultados.getString("nombre");
+	    		 String domicilioSocio = resultados.getString("domicilio");
+	    		 String telefonoSocio = resultados.getString("telefono");
+	    		 String correoSocio = resultados.getString("correo");
+
+	    		 Socio socio = new Socio(codigoSocio, dniSocio, nombreSocio, domicilioSocio, telefonoSocio, correoSocio);
+	    		 todosSocioSP.add(socio);
+
+	    	 }
+	    	 if (todosSocioSP.isEmpty()) {
+	    		 throw new SocioException(SocioException.ERROR_SOCIO_NOPRESTAMO_FECHA);
+	    	 }
+	    }catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return todosSocioSP;
+	}
+	private static boolean esPrestatario(int codigoSocio) throws BDException, SocioException {
+	    PreparedStatement ps = null;
+	    Connection conexion = null;
+	    int resultados = 0;
+	    try {
+	        // Conexión a la base de datos
+	        conexion = ConfigSQLite.abrirConexion();
+	        String query = "SELECT * FROM socio JOIN prestamo ON (socio.codigo = prestamos.codigo_socio) WHERE socio.codigo = ?";
+
+	        ps = conexion.prepareStatement(query);
+	        ps.setInt(1,codigoSocio);
+
+	        resultados = ps.executeUpdate();
+	    } catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return resultados == 1;
+	}
+	
+	public static ArrayList<Socio> consultarSociosPorLocalidadOrdenadosPorNombre(String localidad) throws BDException {
+		PreparedStatement ps = null;
+	    Connection conexion = null;
+	    ArrayList<Socio> socios = new ArrayList<>();
+ 
+	    try {
+	       conexion = ConfigSQLite.abrirConexion();
+	       String query = "SELECT * FROM socio "
+                    + "WHERE lower(domicilio) LIKE lower(?) "
+                    + "AND lower(domicilio) LIKE lower(?) "
+                    + "ORDER BY nombre";
+	       
+	       ps = conexion.prepareStatement(query);
+	
+	       ps.setString(1, "%," + localidad + "%");
+        ps.setString(2, "%, " + localidad + "%");
+
+	        ResultSet resultados = ps.executeQuery();
+	        
+	        while (resultados.next()) {
+	        	socios.add(new Socio(
+        			resultados.getInt("codigo"),
+	        		resultados.getString("dni"),
+	        		resultados.getString("nombre"),
+	        		resultados.getString("domicilio"),
+	        		resultados.getString("telefono"),
+	        		resultados.getString("correo")
+    			));
+	        }
+	    } catch (SQLException e) {
+	        throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+	    } finally {
+	        if (conexion != null) {
+	            ConfigSQLite.cerrarConexion(conexion);
+	        }
+	    }
+	    return socios;
+	}
+	
+	/**
+	 * metodo hecho por juan para un metodo de prestamo. no poner en el main si no se necesita
+	 * @param codigoSocio
+	 * @return
+	 * @throws BDException
+	 */
+	public static Socio consultarSocioPorCodigo(int codigoSocio) throws BDException {
+		Socio socio = null;
+		
+		Connection conexion = null;
+        try {
+            conexion = ConfigSQLite.abrirConexion();
+            
+            String query = "select * from socio where codigo = ?";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, codigoSocio);
+            
+            ResultSet resultados = ps.executeQuery();
+            
+            if (resultados.next()) {
+            	socio = new Socio(
+            		codigoSocio,
+            		resultados.getString("dni"),
+            		resultados.getString("nombre"),
+            		resultados.getString("domicilio"),
+            		resultados.getString("telefono"),
+            		resultados.getString("correo")
+    			);
+            }
+            
+        } catch (SQLException e) {
+            throw new BDException(BDException.ERROR_QUERY + e.getMessage());
+        } finally {
+            if (conexion != null) {
+                ConfigSQLite.cerrarConexion(conexion);
+            }
+        }
+        
+        return socio;
+	}
 }
-// 6. CONSULTAR SOCIOS CON PRESTAMOS
 
-/*
- * public static void main(String[] args) { try { String localidadString =
- * Teclado.leerCadena("Localidad?"); ArrayList<Socio> arraySocios =
- * consultarSociosLocalidad(localidadString); if (arraySocios.isEmpty()) {
- * System.out.println("La lista de socios esta vacia"); }else { for (Socio socio
- * : arraySocios) { System.out.println(socio.toString()); } }
- * 
- * } catch (BDException e) { // TODO Auto-generated catch block
- * e.printStackTrace(); } }
- */
